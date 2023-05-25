b52ed3879813fafe722b028b19bd8e455fdf3126
ewanb1234567890 <114257587+ewanb1234567890@users.noreply.github.com>
2023-05-25 12:01:23 -0400

    un-lock websites.js

--- un-lock websites.js
+++ un-lock websites.js
@@ -1,5 +1,6 @@
 javascript: (() => {
   alert('This bookmarklet is currently in the process of being made.');
   prompt("Welcome to Orginizu Un-locked, what is your name?");
-  createElement
+  const button = document.createElement('button')
+  button.innerText = 'Launch Orginizu Un-locked'
 })();
