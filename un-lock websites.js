6dc5d9c6ccaf8ff83c228e80330f5c12126be1ba
ewanb1234567890 <114257587+ewanb1234567890@users.noreply.github.com>
2023-05-26 08:14:39 -0400

    Update un-lock websites.js

--- un-lock websites.js
+++ un-lock websites.js
@@ -1,4 +1,5 @@
 javascript: (() => {
   alert('This bookmarklet is currently in the process of being made.');
   prompt("Welcome to Orginizu Un-locked, what is your name?");
+  window.open('https://www.proxysite.com')
 })();
